6d6162d715a9d42f7559b6269f0070f9963f9640
gameonanil <gameonanil@gmail.com>
2020-06-22 13:07:19 +0545

    first commmit

---
+++ app/src/main/java/com/example/menupractice/PopUpMenu.java
@@ -0,0 +1,14 @@
+package com.example.menupractice;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PopUpMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawer_navigation);
+    }
+}
